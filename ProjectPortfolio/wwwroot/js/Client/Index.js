a7f57d8cbb78a5b0a463fd603bda12c29628d536
Luana Petters F Marchieviskz <luanamarchieviskz@gmail.com>
2024-11-07 19:18:44 -0300

    Clean Client Ok

--- ProjectPortfolio/wwwroot/js/Client/Index.js
+++ ProjectPortfolio/wwwroot/js/Client/Index.js
@@ -1,6 +1,7 @@
-﻿$(document).ready(function () {
-    alert("sss");
-});
+﻿window.onload = function () {
+    $("#clientInfoId").mask("999.999.99-99");
+    $("#phoneNumberId").mask("(999)99999-9999");
+};
 
 function ChangePeople(bool) {
     if (bool) {
@@ -14,3 +15,21 @@ function ChangePeople(bool) {
         $("#surnameId").prop('disabled', true);
     }
 } 
+
+function CleanEdit() {
+    $("#clientId").val("");
+    $("#cpfId").prop('checked', true);
+    $("#clientInfoId").mask("999.999.99-99");
+    $("#clientInfoId").val("");
+    $("#nameId").val("");
+    $("#surnameId").val("");
+    $("#surnameId").prop('disabled', false);
+    $("#nameId").val("");
+    $("#phoneNumberId").val("");
+    $("#mailId").val("");
+    $("#zipCodeId").val("");
+    $("#addressId").val("");
+    $("#cityId").val("");
+    $("#stateId").val("");
+    $("#isEnabledId").prop('checked', true);    
+}
