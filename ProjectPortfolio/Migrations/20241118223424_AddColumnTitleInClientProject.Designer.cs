99c915cb86d6e66b3023163c231de672e5d9d583
luanapettersm <91229163+luanapettersm@users.noreply.github.com>
2024-11-18 19:35:13 -0300

    Migration para aplicar alteração na tabela de projeto do cliente

--- ProjectPortfolio/Migrations/20241118223424_AddColumnTitleInClientProject.Designer.cs
+++ ProjectPortfolio/Migrations/20241118223424_AddColumnTitleInClientProject.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjectPortfolio.Data;
 
@@ -11,9 +12,11 @@ using ProjectPortfolio.Data;
 namespace ProjectPortfolio.Migrations
 {
     [DbContext(typeof(Repository))]
-    partial class RepositoryModelSnapshot : ModelSnapshot
+    [Migration("20241118223424_AddColumnTitleInClientProject")]
+    partial class AddColumnTitleInClientProject
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ProjectPortfolio/Migrations/20241118223424_AddColumnTitleInClientProject.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectPortfolio.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColumnTitleInClientProject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "SystemUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "ClientProjects",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "SystemUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "ClientProjects");
+        }
+    }
+}
