867a83d04038740aaf4231f35dfa015a848aeb1d
luanapettersm <91229163+luanapettersm@users.noreply.github.com>
2024-11-24 15:50:09 -0300

    Criado Model de autenticação;

---
+++ ProjectPortfolio/Models/AuthenticateModel.cs
@@ -0,0 +1,13 @@
+﻿using ProjectPortfolio.Enumerators;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectPortfolio.Models
+{
+    [NotMapped]
+    public class AuthenticateModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public SystemRoleEnum Roles { get; set; }
+    }
+}
