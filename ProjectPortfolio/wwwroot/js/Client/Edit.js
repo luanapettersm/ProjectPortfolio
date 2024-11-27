5c0fb2d756df283a6e4db02d74eabd622d7eca6e
Luana Petters F Marchieviskz <luanamarchieviskz@gmail.com>
2024-11-27 01:15:36 -0300

    Alert info

--- ProjectPortfolio/wwwroot/js/Client/Edit.js
+++ ProjectPortfolio/wwwroot/js/Client/Edit.js
@@ -45,6 +45,32 @@ function Save() {
     $.post(`/Client/Save`, fd)
         .done(function (response) {
             $('#wrapper-edit').html("");
+            AlertSaveSuccess();
             Filter();
-        })
+        }).fail(function (response) {
+            AlertSaveError(response);
+        });
+
+}
+
+function AlertSaveSuccess() {
+    $("#wrapper-alert").html(`
+         <div class="alert alert-success" role="alert">
+            Salvo com sucesso!
+        </div>
+    `)
+    setTimeout(() => {
+        $("#wrapper-alert").html("");
+    }, 3000);
+}
+
+function AlertSaveError(error) {
+    $("#wrapper-alert").html(`
+         <div class="alert alert-danger" role="alert">
+            Erro ao salvar - ${error}
+        </div>
+    `)
+    setTimeout(() => {
+        $("#wrapper-alert").html("");
+    }, 3000);
 }
--- ProjectPortfolio/wwwroot/js/Client/Index.js
+++ ProjectPortfolio/wwwroot/js/Client/Index.js
@@ -15,7 +15,7 @@ function Edit(id) {
     $.get(url, function (response) {
         $("#wrapper-edit").html(response);
         $("#clientInfoId").mask("999.999.99-99");
-        $("#phoneNumberId").mask("(999)99999-9999");
+        $("#phoneNumberId").mask("(999)999999999");
         $("#editModal").show();
     });
 }
@@ -24,5 +24,30 @@ function Delete(id) {
     $.get(`Client/${id}/Delete`)
         .done(function (response) {
             Filter();
+            AlertDeleteSuccess();
+        }).fail(function (response) {
+            AlertDeleteError(response);
         });
+}
+
+function AlertDeteleSuccess() {
+    $("#wrapper-alert").html(`
+         <div class="alert alert-success" role="alert">
+            Deletado com sucesso!
+        </div>
+    `)
+    setTimeout(() => {
+        $("#wrapper-alert").html("");
+    }, 3000);
+}
+
+function AlertDeleteError(error) {
+    $("#wrapper-alert").html(`
+         <div class="alert alert-danger" role="alert">
+            Erro ao deletar - ${error}
+        </div>
+    `)
+    setTimeout(() => {
+        $("#wrapper-alert").html("");
+    }, 3000);
 }
