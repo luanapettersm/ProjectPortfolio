906550dcf181a7642fc85fd3dfa42659613a464f
Luana Petters F Marchieviskz <luanamarchieviskz@gmail.com>
2024-11-28 00:18:47 -0300

    Correção

--- ProjectPortfolio/wwwroot/js/Client/Index.js
+++ ProjectPortfolio/wwwroot/js/Client/Index.js
@@ -8,7 +8,7 @@
             class: 'text-left',
             orderable: false,
             render: function (data, type, row) {
-                return row.cnpj == "" ? row.CPFFormatado : row.CNPJFormatado;
+                return row.CNPJFormatado === "" ? row.CPFFormatado : row.CNPJFormatado;
             }
         },
         {
