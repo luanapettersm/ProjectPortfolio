d4561e7148a715d27a4d416564d71124bc1bd3cd
luanapettersm <91229163+luanapettersm@users.noreply.github.com>
2024-11-27 22:31:45 -0300

    identado

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,6 @@
 Cypress.Commands.addAll({
     Login(email, password) {
         cy.visit('/')
-
         cy.get('#input-1').type(email)
         cy.get('#input-3').type(password)
         cy.get('.v-btn').contains('Entrar').click().wait(3500);
