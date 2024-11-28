98155ed2fcdb57dfa3960ce7eeb61d40378df7c6
luanapettersm <91229163+luanapettersm@users.noreply.github.com>
2024-11-27 22:04:41 -0300

    removido trecho comentadp

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,15 +5,5 @@ Cypress.Commands.addAll({
         cy.get('#input-1').type(email)
         cy.get('#input-3').type(password)
         cy.get('.v-btn').contains('Entrar').click().wait(3500);
-    },
-    //SelectInput(label, option, position = 0) {
-    //    cy.get([label = "${label}"][aria - owns]: visible).then((select) => {
-    //        if (position < 0) position = select.length - position;
-    //        else if (select.length <= position) position = select.length - 1;
-    //        cy.get(select).eq(position).click().invoke('attr', 'aria-owns').as('response');
-    //        cy.get('@response').then((response) => {
-    //            cy.get('#' + response).contains(option).click();
-    //        });
-    //    });
-    //},
+    }
 }) 
