85149cf7fc7e130d7bf11a1020d89bc9a61061da
luanapettersm <91229163+luanapettersm@users.noreply.github.com>
2024-11-29 02:38:15 -0300

    Ajuste da controller

--- ProjectPortfolio/Models/CreateTicketModel.cs
+++ ProjectPortfolio/Models/CreateTicketModel.cs
@@ -5,9 +5,9 @@ namespace ProjectPortfolio.Models
     [NotMapped]
     public class CreateTicketModel
     {
-        public List<ClientModel> Clients { get; set; }
-        public List<SystemUserModel> Attendants { get; set; }
-        public List<ClientProjectModel> Projects { get; set; }
+        public IEnumerable<ClientModel> Clients { get; set; }
+        public IEnumerable<SystemUserModel> Attendants { get; set; }
+        public IEnumerable<ClientProjectModel> Projects { get; set; }
         public IssueModel Issue { get; set; }
     }
 }
