b177ea624e24037ef1ae2bb4536d3675f8862ae2
luanapettersm <91229163+luanapettersm@users.noreply.github.com>
2024-11-30 12:59:05 -0300

    teste

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,16 +1,7 @@
 Cypress.Commands.addAll({
     Login(username, password) {
         cy.visit('/')
-        cy.get(':nth-child(2) > .nav-link').click()
-        cy.get(':nth-child(1) > :nth-child(5) > [title="Projetos"]').click().should('be.visible')
-        cy.get('.modal-body > .d-flex > .btn').click()
-        cy.get('#projectId').click().type('Novo projeto para o cliente')
-        cy.get('#descriptionId').click().type('Descrição do novo projeto do cliente')
-        cy.get('#addressId').click().type('Address')
-        cy.get('#numberId').click().type('25')
-        cy.get('#cityId').clear().type('City')
-        cy.get('#isEnabledId').click()
-        cy.get('.mb-1').click()
+
         cy.get('#input-1').type(username)
         cy.get('#input-3').type(password)
         cy.get('.v-btn').contains('Entrar').click().wait(3500);
