cee4104abaff41bbafe1612c80eeade1ab83d157
luanapettersm <91229163+luanapettersm@users.noreply.github.com>
2024-11-30 12:14:09 -0300

    correção pipe sonar

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -2,7 +2,7 @@ import './commands'
 
 beforeEach(() => {
     cy.fixture('auth').then(auth => {
-        const { email, password } = auth
-        cy.Login(email, password)
+        const { username, password } = auth
+        cy.Login(username, password)
     })
 })
